80d9c20b8f38f86818c32850a272d56afbb33700
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2026-02-03 21:38:33 -0800

    setting Employee class structure

---
+++ ConsoleApp2/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Employee
+    {
+        public string nom;
+        public string type_employee;
+
+        public string TypeEmployee
+        {
+            get { return type_employee; }
+            set {  type_employee = value; }
+        }
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public Employee(string nom, string type_employee)
+        {
+            this.nom = nom;
+            this.type_employee = type_employee;
+        }
+    }
+}
