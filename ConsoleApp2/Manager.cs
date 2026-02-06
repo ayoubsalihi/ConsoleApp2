c63fd688e5905920c16fd525a116d22e8968a332
Ayoub Salihi <ayoubsqlehi50@gmail.com>
2026-02-05 23:52:12 -0800

    completing the technical architecture

---
+++ ConsoleApp2/Manager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal sealed class Manager : Employee
+    {
+        private int membres_equipe;
+        private float salaire;
+
+        //constructeur
+        public Manager(string nom, string type_employee, int membres_equipe, float salaire)
+            : base(nom, type_employee)
+        {
+            this.membres_equipe = membres_equipe;
+            this.salaire = salaire;
+        }
+        // getters et setters
+        public int MembresEquipes
+        {
+            get
+            {
+                return membres_equipe;
+            }
+            set
+            {
+                membres_equipe = value;
+            }
+        }
+
+        public float Salaire
+        {
+            get
+            {
+                return salaire;
+            }
+            set
+            {
+                salaire = value;
+            }
+        }
+
+        public override void Afficher()
+        {
+            base.Afficher();
+            Console.WriteLine($"Salaire: {salaire}, Membres d'équipe: {membres_equipe}");
+        }
+    }
+}
---
+++ ConsoleApp2/Temporary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal sealed class Temporary : Employee
+    {
+        public int taux_horaire;
+        public int heurestravaillees;
+
+        //constructeur
+        public Temporary(string nom, string type_employee, int taux_horaire, int heurestravaillees)
+            : base(nom, type_employee)
+        {
+            this.taux_horaire = taux_horaire;
+            this.heurestravaillees = heurestravaillees;
+        }
+        /*je ne veux pas développer la partie getters et setters
+         * c'est pour ça j'ai mis les attributs publique */
+
+        public override void Afficher()
+        {
+            base.Afficher();
+            Console.WriteLine($"Taux horaires: {taux_horaire}, heures travaillées");
+        }
+    }
+}
